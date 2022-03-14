9eb7261d25463a32b18a4e9e99f9fbe8085512a1
tam-sal <t.saleh.gc@gmail.com>
2022-03-14 00:13:24 -0300

    "Saving Tabs Using Chrome API"

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+    
+})
 
 deleteBtn.addEventListener('dblclick', function(){
     localStorage.clear()
